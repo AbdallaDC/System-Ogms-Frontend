43ae13ca282693deb5f01d4b9550bf625dc3939d
mohamedkhaliif <qaabturfilms@gmail.com>
2025-08-15 11:11:29 +0300

    First commit

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "https://online-garage-backend.onrender.com";
-// export const API_BASE_URL = "http://localhost:8800";
+// export const API_BASE_URL = "https://online-garage-backend.onrender.com";
+export const API_BASE_URL = "http://localhost:8800";
